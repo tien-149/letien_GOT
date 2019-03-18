dd5e8641eacef771ee086642e226f083ae7e4c3b
Le <t_le86524@tss-hr420-dm20.local>
2019-03-18 16:00:20 -0400

    animation for banner

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -1,25 +1,55 @@
 (() => {
-	console.log('fired');
+	// stub
+	console.log('fired!');
 
+	const 	sigils 		= document.querySelectorAll('.sigil-container'),
+			lightbox 	= document.querySelector('.lightbox'),
+			video		= document.querySelector('video');
+			closeLB     = document.querySelector('.lightbox-close');
+			banners		= document.querySelector('#houseImages');
 
-// Grab the sheildsat the bottom of the page
-	const shields = document.querySelectorAll('.sigil-container'),
-		  lightBox = document.querySelector('.lightbox'),
-		  video = document.querySelector('video');
+	// function slideIMG() {
+	// 	let targetIMG = this.id();
+	// 	let targetImages = targetIMG; 
+	// 	image.src = `images/${}.jpg`;
+	// 	image.show();
+	// }
+
 	function openLightbox() {
-		// lightBox.classList.add('show-lightbox');
-		lightBox.classList.add('lightbox-on');
+		//Grap the right video source
+		// debugger;
+		//Let the lowercase house namw form the class List
+		let targetHouse =this.className.split(" ")[1];
+		console.log(targetHouse);
+
+
+		// stack become Stark -> first make a capital S, then add ark or any house name
+		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		video.src = `video/House-${targetSrc}.mp4`;
+
+		lightbox.classList.add('lightbox-on');
+		video.load();
 		video.play();
 
 	}
+
 	function closeLightbox() {
-		// lightBox.classList.remove('show-lightbox');
-		lightBox.classList.remove('lightbox-on');
+		lightbox.classList.remove('lightbox-on');
 		video.currentTime = 0;
 		video.pause();
 	}
+	function animateBanner() {
+		const offSet = 600; //thus us the offset / width of one image
 
-	shields.forEach(sigil => sigil.addEventListener('click', openLightbox));
+		//this is the total distance the images need to mive as a puxel value 
+		//dataset.offset is comming from each sheild we click on 
+		totalOffset = this.dataset.offset * offSet + "px";
+		banners.style.right = totalOffset;
+	}
+	// sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+	sigils.forEach(sigil => sigil.addEventListener('click', animateBanner));
 
 	video.addEventListener('ended', closeLightbox);
+	closeLB.addEventListener('click', closeLightbox);
 })();
